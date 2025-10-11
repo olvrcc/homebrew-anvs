7345e895ccfd3e5cc8c870d70c0073d916c11f58
Cameron Olivier <cameronolivier@gmail.com>
2025-10-11 17:05:46 +0200

    chore: update URLs after xvn repo transfer to olvrcc org

--- Formula/xvn.rb
+++ Formula/xvn.rb
@@ -1,15 +1,15 @@
 class Xvn < Formula
   desc "Automatic Node.js version switching - 2-3x faster than avn"
-  homepage "https://github.com/cameronolivier/xvn"
+  homepage "https://github.com/olvrcc/xvn"
   version "1.4.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/cameronolivier/xvn/releases/download/v1.4.1/xvn-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/olvrcc/xvn/releases/download/v1.4.1/xvn-aarch64-apple-darwin.tar.gz"
       sha256 "27c643413774e73dd96eb892eb7d2ae62d74321da19e18a82c0d096e0204c7f3"
     else
-      url "https://github.com/cameronolivier/xvn/releases/download/v1.4.1/xvn-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/olvrcc/xvn/releases/download/v1.4.1/xvn-x86_64-apple-darwin.tar.gz"
       sha256 "6a207c38a7297cb91c583eb377d9262ce7ed7eaf5f73621dddec0d35a93c1e53"
     end
   end
